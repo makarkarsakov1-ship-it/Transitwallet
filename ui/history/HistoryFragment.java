989a636fab3f3286584b26ea97b1fdbfba78f715
makarkarsakov1-ship-it <makarkarsakov1@gmail.com>
2026-06-02 19:23:31 +0700

    Create HistoryFragment.java

---
+++ ui/history/HistoryFragment.java
@@ -0,0 +1,21 @@
+package com.transitwallet.app.ui.history;
+
+import android.os.Bundle;
+import android.view.*;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.transitwallet.app.R;
+
+public class HistoryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_history,
+            container, false);
+    }
+}
